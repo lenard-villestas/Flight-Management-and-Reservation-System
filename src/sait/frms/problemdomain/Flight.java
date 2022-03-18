9976b422efd9938c7c9521cf5887a929a5fd1083
patricko5 <64801567+patricko5@users.noreply.github.com>
2022-03-17 20:04:02 -0600

    Getters and Setters
    READ ME AT LINE 96 TO 99

--- src/sait/frms/problemdomain/Flight.java
+++ src/sait/frms/problemdomain/Flight.java
@@ -1,23 +1,101 @@
 package sait.frms.problemdomain;
 
 public class Flight {
-	private String flightName;
-
-	public void getFlightName() {
-
+	private String code;
+	private String airlineName;
+	private String from;
+	private String to;
+	private String weekday;
+	private String time;
+	private int seats;
+	private double costPerSeat;
+	
+	public Flight() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
-	public void getFlightNumber() {
+	public Flight(String code, String airlineName, String from, String to, String weekday, String time, int seats,
+			double costPerSeat) {
+		super();
+		this.code = code;
+		this.airlineName = airlineName;
+		this.from = from;
+		this.to = to;
+		this.weekday = weekday;
+		this.time = time;
+		this.seats = seats;
+		this.costPerSeat = costPerSeat;
 	}
 
-	public void getAirline() {
+	public String getCode() {
+		return code;
 	}
 
-	public void getFlightPositive() {
+	public void setCode(String code) {
+		this.code = code;
 	}
 
-	public void getTicketPrice() {
-
+	public String getAirlineName() {
+		return airlineName;
 	}
 
+	public void setAirlineName(String airlineName) {
+		this.airlineName = airlineName;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getWeekday() {
+		return weekday;
+	}
+
+	public void setWeekday(String weekday) {
+		this.weekday = weekday;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getSeats() {
+		return seats;
+	}
+
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+
+	public double getCostPerSeat() {
+		return costPerSeat;
+	}
+
+	public void setCostPerSeat(double costPerSeat) {
+		this.costPerSeat = costPerSeat;
+	}
+	
+	/*
+	 * I've created the getters for some getters,
+	 * Missing methods:  
+	 * isActive(), setName(), setName(name), setCitizen(), setActive(), toString()
+	 */
+	
 }
