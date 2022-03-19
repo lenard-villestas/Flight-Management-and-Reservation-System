cfc3701c162ddade6f56bb58ebc3e0f5ff662d5d
lenard <lenard.villestas@outlook.com>
2022-03-19 00:55:55 -0600

    problemdomain
    flight and reservation models

--- src/sait/frms/problemdomain/Flight.java
+++ src/sait/frms/problemdomain/Flight.java
@@ -13,24 +13,12 @@ public class Flight {
 	private String time;
 	private int seats;
 	private double costPerSeat;
-<<<<<<< HEAD
+	private boolean domestic;
 	
 	public Flight() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Flight(String code, String airlineName, String from, String to, String weekday, String time, int seats,
-			double costPerSeat) {
-=======
-	private boolean domestic;
-	/**
-	 * Default Constructor
-	 */
-	public Flight() {
-		super();
-	}
-	
 	
 	/**
 	 * @param code
@@ -45,51 +33,12 @@ public class Flight {
 	 */
 	public Flight(String code, String airlineName, String from, String to, String weekday, String time, int seats,
 			double costPerSeat, boolean domestic) {
->>>>>>> Lenard_PDomain
-		super();
-		this.code = code;
-		this.airlineName = airlineName;
-		this.from = from;
-		this.to = to;
-		this.weekday = weekday;
-		this.time = time;
-		this.seats = seats;
-		this.costPerSeat = costPerSeat;
-<<<<<<< HEAD
 	}
 
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
+	private void parseCode(String code){
 		this.code = code;
 	}
-
-	public String getAirlineName() {
-		return airlineName;
-	}
-
-	public void setAirlineName(String airlineName) {
-		this.airlineName = airlineName;
-	}
-
-	public String getFrom() {
-		return from;
-	}
-
-=======
-		this.domestic = domestic;
-	}
-
-
-	/**
-	 * Parse code ??
-	 * @param code
-	 */
-	private void parseCode(String code) {
-		setCode(code);
-	}
+	
 	/**
 	 * @return the code
 	 */
@@ -147,6 +96,13 @@ public class Flight {
 	}
 
 	/**
+	 * @return the domestic
+	 */
+	public boolean isDomestic() {
+		return domestic;
+	}
+
+	/**
 	 * @param code the code to set
 	 */
 	public void setCode(String code) {
@@ -163,101 +119,52 @@ public class Flight {
 	/**
 	 * @param from the from to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setFrom(String from) {
 		this.from = from;
 	}
 
-<<<<<<< HEAD
-	public String getTo() {
-		return to;
-	}
-
-=======
 	/**
 	 * @param to the to to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setTo(String to) {
 		this.to = to;
 	}
 
-<<<<<<< HEAD
-	public String getWeekday() {
-		return weekday;
-	}
-
-=======
 	/**
 	 * @param weekday the weekday to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setWeekday(String weekday) {
 		this.weekday = weekday;
 	}
 
-<<<<<<< HEAD
-	public String getTime() {
-		return time;
-	}
-
-=======
 	/**
 	 * @param time the time to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setTime(String time) {
 		this.time = time;
 	}
 
-<<<<<<< HEAD
-	public int getSeats() {
-		return seats;
-	}
-
-=======
 	/**
 	 * @param seats the seats to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setSeats(int seats) {
 		this.seats = seats;
 	}
 
-<<<<<<< HEAD
-	public double getCostPerSeat() {
-		return costPerSeat;
-	}
-
-=======
 	/**
 	 * @param costPerSeat the costPerSeat to set
 	 */
->>>>>>> Lenard_PDomain
 	public void setCostPerSeat(double costPerSeat) {
 		this.costPerSeat = costPerSeat;
 	}
-	
-<<<<<<< HEAD
-	/*
-	 * I've created the getters for some getters,
-	 * Missing methods:  
-	 * isActive(), setName(), setName(name), setCitizen(), setActive(), toString()
-	 */
-	
-	/*
-	 * Test 12
-	 */
-=======
+
 	/**
-	 * @return the isDomestic boolean
+	 * @param domestic the domestic to set
 	 */
-	public boolean isDomestic() {
-		
-		return domestic;
+	public void setDomestic(boolean domestic) {
+		this.domestic = domestic;
 	}
 
-	
 	@Override
 	public String toString() {
 		return "Flight [code=" + code + ", airlineName=" + airlineName + ", from=" + from + ", to=" + to + ", weekday="
@@ -271,7 +178,5 @@ public class Flight {
 	
 	
 	
-	
->>>>>>> Lenard_PDomain
-	
+
 }
